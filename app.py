2a201f207b9676cd2569b382a0121c221bab6c28
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-10 14:28:39 +0100

    QR

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st 
-import segno
 
-qrcode_1 = segno.make_qr ("Hello World")
+import qrcode
 
+# Create an instance of the QRCode class
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+data = "Hello, QR Code!"
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save or display the image
+img.save("my_qrcode.png")
+img.show()
