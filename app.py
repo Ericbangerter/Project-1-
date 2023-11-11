7ae90a3feccd1d0bc46d37d66ae3f49ba26745a6
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-11 20:47:01 +0100

    QR

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-import streamlit as st 
-pip install qrcode[pil]
-import qrcode
+%pip install streamlit
+%pip install python-barcode
 
-# Create an instance of the QRCode class
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
+import streamlit as st
+import barcode
+from barcode.writer import ImageWriter
 
-# Add data to the QR code
-data = "Hello, QR Code!"
-qr.add_data(data)
-qr.make(fit=True)
+# Function to generate and display a barcode
+def generate_and_display_barcode(data):
+    code = barcode.get('code128', data, writer=ImageWriter(), add_checksum=False)
+    barcode_image = code.save('temp_barcode')
+    st.image('temp_barcode.png', use_column_width=True)
 
-# Create an image from the QR code
-img = qr.make_image(fill_color="black", back_color="white")
+# Streamlit app
+def main():
+    st.title("Barcode Generator")
+    
+    # User input for data to encode
+    data_to_encode = st.text_input("Enter data to encode:")
+    
+    if st.button("Generate Barcode"):
+        if data_to_encode:
+            generate_and_display_barcode(data_to_encode)
+        else:
+            st.warning("Please enter data to encode.")
 
-# Save or display the image
-img.save("my_qrcode.png")
-img.show()
+if __name__ == "__main__":
+    main()
